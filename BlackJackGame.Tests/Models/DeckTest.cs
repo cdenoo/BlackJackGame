fc9a6a788fb907ab9cc5dc74713a46f2699d8b67
cdenoo <9006411+cdenoo@users.noreply.github.com>
2018-10-05 11:23:31 +0200

    Deck en DeckTest geimplementeerd

--- BlackJackGame.Tests/Models/DeckTest.cs
+++ BlackJackGame.Tests/Models/DeckTest.cs
@@ -1,21 +1,43 @@
 ﻿using Xunit;
 using BlackJackGame.Models;
+using System;
 
 namespace BlackJackGame.Tests.Models
 {
     public class DeckTest
     {
-        public Deck _deck;
+        private Deck _deck;
 
         public DeckTest()
         {
-
+            _deck = new Deck();
         }
 
         [Fact]
-        public void DrawReturnsBlackJackCard()
+        public void Draw_ReturnsBlackJackCard()
         {
+            Assert.IsType<BlackJackCard>(_deck.Draw());
+        }
 
+        [Fact]
+        public void Deck_ConstructorAdds52BlackJackCards()
+        {
+            int count = 0;
+            foreach(BlackJackCard card in _deck.Cards)
+            {
+                count++;
+            }
+            Assert.Equal(52, count);
+        }
+
+        [Fact]
+        public void Draw_ThrowsInvalidOperationException_WhenDeckEmpty()
+        {
+            for(int i = 0; i<52; i++)
+            {
+                _deck.Draw();
+            }
+            Assert.Throws<InvalidOperationException>(()=>_deck.Draw());
         }
     }
 }
--- BlackJackGame/Models/Deck.cs
+++ BlackJackGame/Models/Deck.cs
@@ -9,14 +9,32 @@ namespace BlackJackGame.Models
         private Random _random;
         private IList<BlackJackCard> _cards;
 
+        public IEnumerable<BlackJackCard> Cards { get; }
+
         public Deck()
         {
-            throw new NotImplementedException();
+            _random = new Random();
+            _cards = new List<BlackJackCard>();
+            Cards = _cards;
+            
+            for(int suit = 1; suit <= 4; suit++)
+            {
+                for(int value = 1; value <= 13; value++)
+                {
+                    _cards.Add(new BlackJackCard((Suit)suit, (FaceValue)value));
+                }
+            }
         }
 
         public BlackJackCard Draw()
         {
-            throw new NotImplementedException();
+            if(_cards.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot draw from empty deck");
+            }
+            BlackJackCard card = _cards[0];
+            _cards.RemoveAt(0);
+            return card;
         }
 
         public void Shuffle()
